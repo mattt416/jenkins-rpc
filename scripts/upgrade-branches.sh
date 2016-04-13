83b5abffcdb3bb03bf3f48e44f263eea9c38e5c0
Hugh Saunders <hugh@wherenow.org>
2016-04-13 16:18:37 +0100

    Find all paths when no branch specified

--- scripts/upgrade-branches.sh
+++ scripts/upgrade-branches.sh
@@ -16,88 +16,103 @@ function check_tag {
     echo $tag_to_test
   fi
 }
-current_branch=$1
 
-closest_tag=$(git describe --tags --abbrev=0 origin/$current_branch)
+function find_paths(){
+  current_branch="$1"
+  closest_tag=$(git describe --tags --abbrev=0 origin/$current_branch)
 
-original_ref=$(git rev-parse 'HEAD^{commit}')
+  original_ref=$(git rev-parse 'HEAD^{commit}')
 
-git checkout --quiet master
-git submodule --quiet update --init
-pushd openstack-ansible > /dev/null
-  osa_t=$(check_tag $(git describe --abbrev=0))
-popd > /dev/null
-git checkout --quiet $original_ref
-git submodule --quiet update --init
+  git checkout --quiet master
+  git submodule --quiet update --init
+  pushd openstack-ansible > /dev/null
+    osa_t=$(check_tag $(git describe --abbrev=0))
+  popd > /dev/null
+  git checkout --quiet $original_ref
+  git submodule --quiet update --init
 
-osa_major_number=$(echo "$osa_t" | cut -d. -f1)
-# Assume rpc-openstack major version is the same as OpenStack-Ansible tag major version.
-master_major_number=$osa_major_number
+  osa_major_number=$(echo "$osa_t" | cut -d. -f1)
+  # Assume rpc-openstack major version is the same as OpenStack-Ansible tag major version.
+  master_major_number=$osa_major_number
 
-if [[ $current_branch == 'master' ]]; then
-  # If the branch/rc tag process was not followed correctly closest_tag might not show what was expected
-  #c_tag=($(echo $closest_tag | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | cut -d. --output-delimiter=' ' -f1-))
-  c_tag=($(git tag --list r[0-9]*| sort -V | tail -n1 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | tr '.' ' '))
-  branch_major_number=${c_tag[0]}
-  if [[ ${c_tag[0]} == $master_major_number ]]; then
-    # master should be for the next minor after the one pointed to be closest_tag
-    branch_minor_number=$(expr ${c_tag[1]} + 1)
+  if [[ $current_branch == 'master' ]]; then
+    # If the branch/rc tag process was not followed correctly closest_tag might not show what was expected
+    #c_tag=($(echo $closest_tag | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | cut -d. --output-delimiter=' ' -f1-))
+    c_tag=($(git tag --list r[0-9]*| sort -V | tail -n1 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' | tr '.' ' '))
+    branch_major_number=${c_tag[0]}
+    if [[ ${c_tag[0]} == $master_major_number ]]; then
+      # master should be for the next minor after the one pointed to be closest_tag
+      branch_minor_number=$(expr ${c_tag[1]} + 1)
+    else
+      # master should be for a new major
+      branch_major_number=$master_major_number
+      branch_minor_number=0
+    fi
+    current_patch_version=''
   else
-    # master should be for a new major
-    branch_major_number=$master_major_number
-    branch_minor_number=0
+    branch_major_number=$(echo $current_branch | egrep '^[a-z]+-[0-9]+\.[0-9]+$' | cut -d- -f2 | cut -d. -f1)
+    branch_minor_number=$(echo $current_branch | egrep '^[a-z]+-[0-9]+\.[0-9]+$' | cut -d- -f2 | cut -d. -f2)
+    current_patch_version=$(echo $closest_tag | egrep "^r${branch_major_number}\.${branch_minor_number}\.[0-9]+[rc0-9]*$")
   fi
-  current_patch_version=''
+
+  next_minor_version=$(git tag --list r${branch_major_number}.$(expr ${branch_minor_number} + 1).*  | sort -V | tail -n1)
+  if [[ $current_branch != 'master' ]] && [[ $next_minor_version == '' ]] && [[ $master_major_number -eq $branch_major_number ]]; then
+    next_minor_version=master
+  fi
+  next_major_version=$(git tag --list r$(expr ${branch_major_number} + 1).* | sort -V | tail -n1)
+  if [[ $current_branch != 'master' ]] && [[ $next_major_version == '' ]] && [[ $master_major_number -gt $branch_major_number ]]; then
+    next_major_version=master
+  fi
+
+  previous_minor_version=$(check_tag $(git tag --list r${branch_major_number}.$(expr ${branch_minor_number} - 1).* | sort -V | tail -n1))
+  previous_major_version=$(git tag --list r$(expr ${branch_major_number} - 1).* | sort -V | tail -n1)
+
+  >&2 echo "Branch: $current_branch"
+
+  >&2 echo "Closest tag: $closest_tag"
+
+  >&2 echo "Current patch version: $current_patch_version"
+
+  >&2 echo "Next major version: $next_major_version"
+  >&2 echo "Next minor version: $next_minor_version"
+
+  >&2 echo "Previous major version: $previous_major_version"
+  >&2 echo "Previous minor version: $previous_minor_version"
+
+
+  if [[ $current_patch_version != '' ]]; then
+    test_branches="patch-from-$current_patch_version"
+  else
+    test_branches=''
+  fi
+
+  # Assume cannot upgrade to 0 minor release
+  if [[ $previous_major_version != '' ]] && [[ $branch_minor_number != 0 ]]; then
+    test_branches="$test_branches major-from-$previous_major_version"
+  fi
+  if [[ $previous_minor_version != '' ]]; then
+    test_branches="$test_branches minor-from-$previous_minor_version"
+  fi
+
+  # Assume upgrading to first minor not possible
+  if [[ $next_major_version != '' ]] && [[ $next_minor_version != '' ]] && [[ $(echo $next_major_version | egrep -o '\.[0-9]+\.' | cut -d. -f2) != 0 ]]; then
+    test_branches="$test_branches major-to-$next_major_version"
+  elif [[ $next_minor_version != '' ]]; then
+    test_branches="$test_branches minor-to-$next_minor_version"
+  fi
+
+  echo $test_branches
+}
+
+# --- Main ---
+branch="$1"
+if [[ -z "$branch" ]]; then
+  branches="master $(git branch -a |awk -F'[/ ]' '/\/[a-z]+-[0-9]+\.[0-9]+$/{print $NF}' |sort -u)"
 else
-  branch_major_number=$(echo $current_branch | egrep '^[a-z]+-[0-9]+\.[0-9]+$' | cut -d- -f2 | cut -d. -f1)
-  branch_minor_number=$(echo $current_branch | egrep '^[a-z]+-[0-9]+\.[0-9]+$' | cut -d- -f2 | cut -d. -f2)
-  current_patch_version=$(echo $closest_tag | egrep "^r${branch_major_number}\.${branch_minor_number}\.[0-9]+[rc0-9]*$")
+  branches="$branch"
 fi
 
-next_minor_version=$(git tag --list r${branch_major_number}.$(expr ${branch_minor_number} + 1).*  | sort -V | tail -n1)
-if [[ $current_branch != 'master' ]] && [[ $next_minor_version == '' ]] && [[ $master_major_number -eq $branch_major_number ]]; then
-  next_minor_version=master
-fi
-next_major_version=$(git tag --list r$(expr ${branch_major_number} + 1).* | sort -V | tail -n1)
-if [[ $current_branch != 'master' ]] && [[ $next_major_version == '' ]] && [[ $master_major_number -gt $branch_major_number ]]; then
-  next_major_version=master
-fi
-
-previous_minor_version=$(check_tag $(git tag --list r${branch_major_number}.$(expr ${branch_minor_number} - 1).* | sort -V | tail -n1))
-previous_major_version=$(git tag --list r$(expr ${branch_major_number} - 1).* | sort -V | tail -n1)
-
->&2 echo "Branch: $current_branch"
-
->&2 echo "Closest tag: $closest_tag"
-
->&2 echo "Current patch version: $current_patch_version"
-
->&2 echo "Next major version: $next_major_version"
->&2 echo "Next minor version: $next_minor_version"
-
->&2 echo "Previous major version: $previous_major_version"
->&2 echo "Previous minor version: $previous_minor_version"
-
-
-if [[ $current_patch_version != '' ]]; then
-  test_branches="patch-from-$current_patch_version"
-else
-  test_branches=''
-fi
-
-# Assume cannot upgrade to 0 minor release
-if [[ $previous_major_version != '' ]] && [[ $branch_minor_number != 0 ]]; then
-  test_branches="$test_branches major-from-$previous_major_version"
-fi
-if [[ $previous_minor_version != '' ]]; then
-  test_branches="$test_branches minor-from-$previous_minor_version"
-fi
-
-# Assume upgrading to first minor not possible
-if [[ $next_major_version != '' ]] && [[ $next_minor_version != '' ]] && [[ $(echo $next_major_version | egrep -o '\.[0-9]+\.' | cut -d. -f2) != 0 ]]; then
-  test_branches="$test_branches major-to-$next_major_version"
-elif [[ $next_minor_version != '' ]]; then
-  test_branches="$test_branches minor-to-$next_minor_version"
-fi
-
-echo $test_branches
+for branch in $branches
+do
+  echo "$branch: $(find_paths "$branch")"
+done
