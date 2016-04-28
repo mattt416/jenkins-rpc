ee346ff9fae955b4c58299a427b8cf505413205f
Jacob Wagner <swagner1104@gmail.com>
2016-04-28 13:33:23 -0500

    added script to cleanup after tempest if it fails (networks)

---
+++ scripts/qe-labs/cleanup_temepst_networks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Shell Opts ----------------------------------------------------------------
+set -x
+
+# Ensure python-neutronclient is isntalled
+type neutron >/dev/null 2>&1 || { echo >&2 "python-neutronclient is not installed.  Aborting."; exit 1; }
+
+# All tempest neutron networks
+TEMPEST_NETWORKS="$(neutron net-list | grep tempest | awk '{print $2}')"
+
+# All tempest subnets
+TEMPEST_SUBNETS="$(neutron net-list | grep tempest | awk '{print $6}')"
+
+# Remove all tempest ports
+for SUBNET in $TEMPEST_SUBNETS; do
+    for PORT in `neutron port-list | grep $SUBNET | awk '{print $2}'`; do 
+        neutron port-delete $PORT;
+    done
+done
+
+# Remove all tempest networks
+for NET in $TEMPEST_NETWORKS; do neutron net-delete $NET; done
+
+# print all neutron networks
+neutron net-list
