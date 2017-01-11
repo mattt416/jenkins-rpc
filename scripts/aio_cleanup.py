d89998697046930286d7156f2581a5fefc556272
Hugh Saunders <hugh@wherenow.org>
2017-01-11 11:15:05 +0000

    Update AIO Cleanup script
    The AIO cleanup script now reflects our curent AIO deployment naming and
    methodology. Logic related to jenkins slave status has been removed
    because job instances are no longer registerd as slaves.
    
    The old threshold has been increased to 48 hours so that if a job is run
    with keep_instance then the requester has two days to use the instance
    for deugging before its removed.
    
    Also:
     * moves AIO template defaults into a global defaults object
     * Adds parameters to the jenkins-rpc-git macro
    
     Connects rcops/u-suk-dev#953

--- scripts/aio_cleanup.py
+++ scripts/aio_cleanup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+# This script deletes instances whose name starts with "jra" if they are in
+# error state or older than 48hrs.
+
 import datetime
 import dateutil.parser
 import os
-import requests
 
 from novaclient import client
 
@@ -16,47 +18,18 @@ nova = client.Client(
     region_name="IAD"
 )
 
-jenkins_api_user = os.environ['JENKINS_API_USER']
-jenkins_api_pass = os.environ['JENKINS_API_PASS']
-
-slaves = requests.get("http://jenkins.propter.net/computer/api/json",
-                      auth=(jenkins_api_user, jenkins_api_pass)).json()['computer']
-slave_names = [x['displayName'] for x in slaves]
-print("Jenkins Slaves: %s" %slave_names)
-def jenkins_node(name):
-  return name in slave_names
-
-
-slaves=0
-instances=0
 for s in nova.servers.list():
     created = dateutil.parser.parse(s.created)
-    sixhoursago = datetime.datetime.now(created.tzinfo)-datetime.timedelta(hours=6)
-    comingofage = datetime.datetime.now(created.tzinfo)-datetime.timedelta(minutes=7)
-    adult = created < comingofage
+    old_threshold = datetime.datetime.now(
+        created.tzinfo)-datetime.timedelta(hours=48)
     error = s.status == 'ERROR'
-    old = created < sixhoursago
-    is_slave = jenkins_node(s.name)
-    instances+=1
-    if is_slave:
-        slaves+=1
-    print("Instance:{name} slave:{slave} status:{status} adult:{adult} old:{old}".format(
+    old = created < old_threshold
+    print("Instance:{name} status:{status} old:{old}".format(
         name=s.name,
-        slave=is_slave,
         status=s.status,
-        adult=adult,
         old=old
     ))
-    if (s.name.startswith('jrpcaio')
-            and (
-                s.status == 'ERROR'
-                or ((not is_slave) and adult)
-                or old
-            )):
-        print("Deleting %(name)s Error:%(error)s Old:%(old)s"
-              % dict(name=s.name, error=error, old=old))
+    if (s.name.startswith('jra') and (error or old)):
+        print("Deleting {name} Error:{error} Old:{old}".format(
+            name=s.name, error=error, old=old))
         s.delete()
-
-print("Slaves: {numslaves}, Instances that aren't active slaves: {non_slave_instances}".format(
-    numslaves=slaves,
-    non_slave_instances=instances-slaves))
